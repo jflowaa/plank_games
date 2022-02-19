417e584c966809d9c8c8c0971d1406764634251e
jack <jakaf1@gmail.com>
2022-02-19 13:07:09 -0600

    update deploy_vm script

--- deployment/deploy_vm.sh
+++ deployment/deploy_vm.sh
@@ -2,3 +2,7 @@
 
 tar -cvzf plank_games.tar.gz -X deployment/exclude.txt ../plank_games
 scp plank_games.tar.gz root@plank-games:/var/www/html
+ssh plank-games "cd /var/www/html && tar -xvf plank_games.tar.gz"
+ssh plank-games "cd /var/www/html/plank_games && ./deployment/build.sh"
+ssh plank-games "cd /var/www/html/plank_games && _build/prod/rel/plank_games/bin/plank_games stop"
+ssh plank-games "cd /var/www/html/plank_games && _build/prod/rel/plank_games/bin/plank_games daemon"
