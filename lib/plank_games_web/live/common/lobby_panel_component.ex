bae7ca75acfbe6f83016e8cc5f9c41abd0ac4b76
Jack Flowers <jakaf1@gmail.com>
2022-03-12 21:37:45 -0600

    Add slots to lobby panel component

--- lib/plank_games_web/live/common/lobby_panel_component.ex
+++ lib/plank_games_web/live/common/lobby_panel_component.ex
@@ -2,10 +2,18 @@ defmodule PlankGamesWeb.Live.Common.LobbyPanelComponent do
   use Phoenix.LiveComponent
 
   def render(assigns) do
+    assigns =
+      assigns
+      |> assign_new(:extra_buttons, fn -> nil end)
+      |> assign_new(:extra_text, fn -> nil end)
+
     ~H"""
       <div>
         <div class="text-center">
           <p class="font-thin text-lg py-5 dark:text-white">Connected Users: <%= @connection_count %></p>
+          <%= if @extra_buttons do %>
+            <%= render_slot(@extra_buttons) %>
+          <% end %>
           <%= if @show_join do %>
             <button phx-click="join" class="btn-primary" >Join Game</button>
           <% end %>
@@ -23,6 +31,9 @@ defmodule PlankGamesWeb.Live.Common.LobbyPanelComponent do
           <% end %>
           <%= if @has_started do %>
             <h2 class="font-thin text-xl dark:text-white">Current Player: <%= @current_player %></h2>
+          <% end %>
+          <%= if @extra_text do %>
+            <%= render_slot(@extra_text) %>
           <% end %>
         </div>
         <.live_component module={PlankGamesWeb.Live.Common.MessageComponent} id="message" messages={@messages} />
