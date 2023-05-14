4560e9963a30835dfd8e8c8510d2183577912ad7
Jack Flowers <jakaf1@gmail.com>
2023-05-13 20:47:20 -0500

    updates to deployment to vm script

--- deployment/deploy_vm.sh
+++ deployment/deploy_vm.sh
@@ -5,10 +5,10 @@ cd ..
 tar -cvzf plank_games.tar.gz -X plank_games/deployment/exclude.txt plank_games
 scp plank_games.tar.gz root@digital-ocean:/root
 ssh digital-ocean << EOF
+rm -rf plank_games/
 tar -xvf plank_games.tar.gz
-cd plank_games
-docker build . -t plank_games --build-arg MIX_ENV="prod" && \
-  docker stop plank_games && \
-  docker rm plank_games && \
-  docker run -d --name plank_games -e RELEASE_COOKIE=$RELEASE_COOKIE -e SECRET_KEY_BASE=$SECRET_KEY_BASE -p 4000:4000 -it plank_games
+cd plank_games/
+docker build . -t plank-games --build-arg MIX_ENV="prod"
+docker-compose -f deployment/docker-compose.yaml down
+docker-compose -f deployment/docker-compose.yaml up -d
 EOF
